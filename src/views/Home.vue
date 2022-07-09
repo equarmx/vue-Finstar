c7dde7750cf1588fc59c8ab1b34b657b2ef41af7
equarmx <equarmx@gmail.com>
2022-07-10 01:29:43 +0300

    add tailwind for project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="flex justify-center items-center h-screen">
+      <h1 class="font-bold text-9xl">VUE TAILWIND</h1>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: {},
 })
 export default class Home extends Vue {}
 </script>
